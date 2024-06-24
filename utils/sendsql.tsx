bcc6dac85df4c7bbb5b196de98057e477ae2423e
arky02 <kyean07@naver.com>
2024-06-24 18:24:04 +0900

    feat: 배포된 서버 url로 변경

--- utils/sendsql.tsx
+++ utils/sendsql.tsx
@@ -17,16 +17,13 @@ const SendSQLQuery = async ({
 }: SqlProps) => {
   let response = "";
   try {
-    response = await axios.post(
-      `http://ec2-13-125-179-4.ap-northeast-2.compute.amazonaws.com:8080/api/result/save`,
-      {
-        age: age,
-        sex: sex,
-        img_name: img_name,
-        transport_score: transport_score,
-        crime_score: crime_score,
-      }
-    );
+    response = await axios.post(`https://roadvs.shop/api/results/save`, {
+      age: age,
+      sex: sex,
+      img_name: img_name,
+      transport_score: transport_score,
+      crime_score: crime_score,
+    });
   } catch {
     alert("서버 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.");
     return;
